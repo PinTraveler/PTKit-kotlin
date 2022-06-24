4167c34c2610fec1fbab0c672a3f940e95abf295
eneseray <enes.eraay@gmail.com>
2022-06-24 10:15:25 +0300

    huge changes.

--- ptkit/src/main/java/com/pintraveler/ptkit/Exceptions.kt
+++
@@ -1,5 +0,0 @@
-package com.pintraveler.ptkit
-
-class NullObjectException(message: String) : Exception(message)
-class ConflictingParametersException(message: String) : Exception(message)
-class EmptyFieldException(message: String) : Exception(message)
