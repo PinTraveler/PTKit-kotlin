7a9a655b2796ca86f48fd13089d563cb7ed3b56b
Cem Ersoz <cersozz@gmail.com>
2023-01-07 17:11:31 +0300

    delete

--- ptkit/src/main/java/com/pintraveler/ptkit/viewmodels/EmptyCardModel.kt
+++
@@ -1,8 +0,0 @@
-package com.pintraveler.ptkit.viewmodels
-
-import com.pintraveler.ptkit.R
-
-class EmptyCardModel {
-    var imageResource: Int = com.google.android.gms.base.R.drawable.googleg_disabled_color_18
-    var text: String = "Empty Text"
-}
